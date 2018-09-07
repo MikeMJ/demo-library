98e4d8dcfad9e44770a0b3fe5aaa75739db6d2cd
mabdullayev <mabdullayev@azercell.com>
2018-09-07 22:15:34 +0400

    Initial commit.

---
+++ src/main/java/com/mikola/demolibrary/DemoLibraryApplication.java
@@ -0,0 +1,12 @@
+package com.mikola.demolibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLibraryApplication.class, args);
+	}
+}
